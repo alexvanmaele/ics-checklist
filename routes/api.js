df4109e96429eb175fcb0e6435b54eb5e8607548
Alexander Van Maele <alexander.van.maele@hotmail.com>
2016-06-21 15:34:43 +0200

    List services and protocols

--- routes/api.js
+++ routes/api.js
@@ -30,7 +30,7 @@ router.get('/', function(req, res, next)
 {
     res.send('ICS Checklist API - Please use the correct subpath for addressing the API system.');
 });
-// Get all device types
+// Get device types
 router.get('/device-types', function(req, res, next)
 {
     connection.query('SELECT * from types ORDER BY id', function(err, rows, fields)
@@ -40,7 +40,7 @@ router.get('/device-types', function(req, res, next)
         res.send(JSON.stringify(rows, null, 2));
     });
 });
-// Get all vendors
+// Get vendors
 router.get('/vendors', function(req, res, next)
 {
     connection.query('SELECT * from vendors ORDER BY id', function(err, rows, fields)
@@ -50,11 +50,11 @@ router.get('/vendors', function(req, res, next)
         res.send(JSON.stringify(rows, null, 2));
     });
 });
-// Get all vendors per device type
+// Get vendors per device type
 router.get('/vendors/:typeId', function(req, res, next)
 {
     var query = `
-		select vendors.name
+		select vendors.id, vendors.name
 		from devices
 		join vendors on vendors.id = devices.vendor
 		where devices.id in (select dt.device from device_types dt where dt.type = ?)
@@ -66,11 +66,11 @@ router.get('/vendors/:typeId', function(req, res, next)
         res.send(JSON.stringify(rows, null, 2));
     });
 });
-// Get all devices with vendor per device type
+// Get devices with vendor per device type
 router.get('/devices/:typeId', function(req, res, next)
 {
     var query = `
-		select vendors.name as vendor, series.name as series, devices.name as device
+		select devices.id, vendors.name as vendor, series.name as series, devices.name as device
 		from devices
 		join series on series.id = devices.series
 		join vendors on vendors.id = devices.vendor
@@ -84,6 +84,46 @@ router.get('/devices/:typeId', function(req, res, next)
         res.send(JSON.stringify(rows, null, 2));
     });
 });
+// Get services per device
+router.get('/services/:deviceId', function(req, res, next)
+{
+    var query = `
+		select services.id, services.name as service
+		from services
+		join device_services on services.id = device_services.service
+		where device_services.device = ?
+		order by service
+	`;
+    connection.query(query, [req.params.deviceId], function(err, rows, fields)
+    {
+        if (err) console.log('Error in Query');
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(rows, null, 2));
+    });
+});
+// Get protocols per service
+router.get('/protocols/:serviceId', function(req, res, next)
+{
+    var query = `
+		select protocols.id, protocols.name as protocol
+		from protocols
+		join service_protocols on protocols.id = service_protocols.protocol
+		where service_protocols.service = ?
+		order by protocols.name
+	`;
+    connection.query(query, [req.params.serviceId], function(err, rows, fields)
+    {
+        if (err) console.log('Error in Query');
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(rows, null, 2));
+    });
+});
+// Get warnings per protocol
+// TODO
+
+// Get recommendations per warning
+// TODO
+
 /*
 	DEBUG
 */
