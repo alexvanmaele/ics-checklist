1c46d5da2267eaf126b1142771e2a01ffb0739c6
Alexander Van Maele <alexander.van.maele@hotmail.com>
2016-06-21 14:41:32 +0200

    List devices and vendors per device type

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,9 @@ var connection = mysql.createConnection(
     password: 'toor',
     database: 'ics_checklist'
 });
-
+/*
+	SETUP
+*/
 connection.connect(function(err)
 {
     if (!err)
@@ -20,11 +22,15 @@ connection.connect(function(err)
         console.log("Error connecting database ... \n\n");
     }
 });
-
+/*
+	ROUTES
+*/
+// Default route
 router.get('/', function(req, res, next)
 {
     res.send('ICS Checklist API - Please use the correct subpath for addressing the API system.');
 });
+// Get all device types
 router.get('/device-types', function(req, res, next)
 {
     connection.query('SELECT * from types ORDER BY id', function(err, rows, fields)
@@ -34,6 +40,7 @@ router.get('/device-types', function(req, res, next)
         res.send(JSON.stringify(rows, null, 2));
     });
 });
+// Get all vendors
 router.get('/vendors', function(req, res, next)
 {
     connection.query('SELECT * from vendors ORDER BY id', function(err, rows, fields)
@@ -43,6 +50,43 @@ router.get('/vendors', function(req, res, next)
         res.send(JSON.stringify(rows, null, 2));
     });
 });
+// Get all vendors per device type
+router.get('/vendors/:typeId', function(req, res, next)
+{
+    var query = `
+		select vendors.name
+		from devices
+		join vendors on vendors.id = devices.vendor
+		where devices.id in (select dt.device from device_types dt where dt.type = ?)
+	`;
+    connection.query(query, [req.params.typeId], function(err, rows, fields)
+    {
+        if (err) console.log('Error in Query');
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(rows, null, 2));
+    });
+});
+// Get all devices with vendor per device type
+router.get('/devices/:typeId', function(req, res, next)
+{
+    var query = `
+		select vendors.name as vendor, series.name as series, devices.name as device
+		from devices
+		join series on series.id = devices.series
+		join vendors on vendors.id = devices.vendor
+		where devices.id in (select dt.device from device_types dt where dt.type = ?)
+		order by vendors.name
+	`;
+    connection.query(query, [req.params.typeId], function(err, rows, fields)
+    {
+        if (err) console.log('Error in Query');
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(rows, null, 2));
+    });
+});
+/*
+	DEBUG
+*/
 router.get("/test-database", function(req, res)
 {
     connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields)
@@ -58,5 +102,7 @@ router.get("/test-database", function(req, res)
         }
     });
 });
-
+/*
+	EXPORTS
+*/
 module.exports = router;
