255a448797986d38de8bb2013df001c6a1ceb064
Alexander Van Maele <alexander.van.maele@hotmail.com>
2016-07-07 11:11:21 +0200

    Removed default /users/ path

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
 app.use('/api', api);
 
 // catch 404 and forward to error handler
