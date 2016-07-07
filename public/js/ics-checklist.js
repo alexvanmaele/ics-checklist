21c2306b8f94f2f5e7e4502ee0531305907297d2
Alexander Van Maele <alexander.van.maele@hotmail.com>
2016-07-07 11:53:52 +0200

    Added jQuery with load check

--- public/js/ics-checklist.js
+++ public/js/ics-checklist.js
@@ -1,4 +1,12 @@
 (function()
 {
-    console.log('ics-checklist.js loaded');
+	console.log('ics-checklist.js loaded');
+    if (typeof jQuery == 'undefined')
+    {
+    	console.log('jQuery failed to load or still loading...');
+    }
+    $(document).ready(function()
+    {
+    	console.log('jQuery ready!');
+    })
 })();
