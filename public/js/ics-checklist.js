2f02767c1949cd3cb4d8b56a4d796624facfb386
Alexander Van Maele <alexander.van.maele@hotmail.com>
2016-07-07 12:25:05 +0200

    Populate device types list from API

--- public/js/ics-checklist.js
+++ public/js/ics-checklist.js
@@ -1,12 +1,53 @@
 (function()
 {
-	console.log('ics-checklist.js loaded');
+    var urls = {
+        devices: '/api/devices/'
+    };
+    var devices;
+    console.log('ics-checklist.js loaded');
     if (typeof jQuery == 'undefined')
     {
-    	console.log('jQuery failed to load or still loading...');
+        console.log('jQuery failed to load or still loading...');
     }
     $(document).ready(function()
     {
-    	console.log('jQuery ready!');
+        console.log('jQuery ready!');
+        start();
     })
+
+    function start()
+    {
+        loadDevices();
+    }
+
+    function loadDevices()
+    {
+        $.ajax(
+        {
+            dataType: 'json',
+            url: urls.devices,
+            success: function(data)
+            {
+                console.log('Got JSON!');
+                console.log(JSON.stringify(data, null, 2));
+                devices = data;
+                populateDeviceTypeList();
+            }
+        });
+    }
+
+    function populateDeviceTypeList()
+    {
+    	$('#lst_device_types').empty();
+        var deviceTypes = [];
+        for(var i = 0; i < devices.length; i++)
+        {
+        	var type = devices[i].type;
+        	if(deviceTypes.indexOf(type) == -1) deviceTypes.push(type);
+        }
+        $.each(deviceTypes, function(key, type)
+        {
+            $('#lst_device_types').append('<option value=' + type + '>' + type + '</option>');
+        });
+    }
 })();
